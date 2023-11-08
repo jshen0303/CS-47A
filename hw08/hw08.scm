5457848dfe4b243017c1085e876d7e964d48e85a
Jeff Shen <jshen0303@gmail.com>
2023-11-08 11:48:17 -0800

    hw8 complete

--- hw08/hw08.scm
+++ hw08/hw08.scm
@@ -1,15 +1,21 @@
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp)
+  (cond ((= exp 0) 1)
+        ((= exp 1) base)
+        ((even? exp) (square (pow base (/ exp 2))))
+        (else (* base (square (pow base (/ (- exp 1) 2)))))))
 
 (define (repeatedly-cube n x)
   (if (zero? n)
       x
-      (let (_________________________)
+      (let ((y (repeatedly-cube (- n 1) x)))
         (* y y y))))
 
 (define (cddr s) (cdr (cdr s)))
 
-(define (cadr s) 'YOUR-CODE-HERE)
+(define (cadr s)
+  (car (cdr s)))
 
-(define (caddr s) 'YOUR-CODE-HERE)
+(define (caddr s)
+  (car (cdr (cdr s))))
